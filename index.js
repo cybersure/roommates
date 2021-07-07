eda5a179ad1ef1d3a3ee3e87920cf50ad4c34d7a
cybersure <olawaleoyedele@gmail.com>
2021-07-08 00:43:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
+
+if (randomNumber1 > randomNumber2) {
+  zainabWinMessage();
+} else if (randomNumber2 > randomNumber1) {
+  mubaarokaWinMessage();
+} else {
+  drawMessage();
+}
+
+function zainabWinMessage() {
+  var zMessage = "Zainab Wins! Eat Mubaaroka's food."
+  document.querySelector("h1").innerHTML = zMessage;
+}
+
+function mubaarokaWinMessage() {
+  var mMessage = "Mubaaroka Wins! Eat Zainab's food"
+  document.querySelector("h1").innerHTML = mMessage;
+}
+
+function drawMessage() {
+  var dMessage = "Draw! You two bring your food to me &#128512"
+  document.querySelector("h1").innerHTML = dMessage
+}
