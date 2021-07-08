645be29cf30e8c769e8bccc4debf50d0ea6fda80
cybersure <olawaleoyedele@gmail.com>
2021-07-08 13:36:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,24 +9,35 @@ var randomImageSource = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource);
 
 if (randomNumber1 > randomNumber2) {
-  zainabWinMessage();
+  zainabWinMessage(), playerPlaying();
 } else if (randomNumber2 > randomNumber1) {
-  mubaarokaWinMessage();
+  mubaarokaWinMessage(), playerPlaying();
 } else {
-  drawMessage();
+  drawMessage(), playerPlaying();
 }
 
+
+
+
 function zainabWinMessage() {
-  var zMessage = "Zainab Wins! Eat your roommate's food."
+  var zMessage = "Zainab Wins! Eat Mubaaroka's food."
   document.querySelector("h1").innerHTML = zMessage;
 }
 
 function mubaarokaWinMessage() {
-  var mMessage = "Mubaaroka Wins! Eat your roommate's food"
+  var mMessage = "Mubaaroka Wins! Eat Zainab's food"
   document.querySelector("h1").innerHTML = mMessage;
 }
 
 function drawMessage() {
   var dMessage = "Draw! You two bring your food to me &#128512"
   document.querySelector("h1").innerHTML = dMessage
+}
+
+function playerPlaying() {
+  var whoPlaying = ["Zainab Playing", "Mubaaroka Playing"];
+  var choosePlayer = Math.floor(Math.random() * 2);
+  var choose = whoPlaying[choosePlayer];
+  document.querySelector("button").innerHTML = choose;
+
 }
